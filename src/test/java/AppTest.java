4f752d763f16e8c5978bbd90ec5ebe3ee99a5475
A <60852828+testify3@users.noreply.github.com>
2025-10-30 11:43:41 +0100

    Create AppTest.java

---
+++ src/test/java/AppTest.java
@@ -0,0 +1,11 @@
+package com.example;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class AppTest {
+    @Test
+    void testAdd() {
+        assertEquals(3, App.add(1, 2));
+    }
+}
